6b2a71555ae622320b1849a848fa18393dd379b4
Rahul <rahulguptarfj2@gail.com>
2026-09-08 06:45:11 +0530

    Patch API Functionality

--- src/main/java/com/gupta/learningmvcarchitectureofspringboot/service/EmployeeService.java
+++ src/main/java/com/gupta/learningmvcarchitectureofspringboot/service/EmployeeService.java
@@ -6,9 +6,15 @@ import com.gupta.learningmvcarchitectureofspringboot.entities.EmployeeEntity;
 import com.gupta.learningmvcarchitectureofspringboot.repositories.EmployeeRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmployeeService {
@@ -50,13 +56,20 @@ public class EmployeeService {
 
     }
 
+    public boolean IsExist(Long id){
+        return employeeRepository.existsById(id);
+    }
+
 
     public EmployeeDTO updateById(Long id, EmployeeDTO employeeDTO) {
 
         EmployeeEntity employeeEntity = modelMapper.map(employeeDTO, EmployeeEntity.class);
 
-        if (employeeRepository.existsById(id))
+        boolean isexist= IsExist(id);
+
+        if (isexist) {
             employeeEntity.setId(id);
+        }
         EmployeeEntity employeeEntity1 = employeeRepository.save(employeeEntity);
         return modelMapper.map(employeeEntity1, EmployeeDTO.class);
 
@@ -64,9 +77,34 @@ public class EmployeeService {
     }
 
     public boolean deleteById(Long id) {
-        if(!employeeRepository.existsById(id))
+        boolean isexist= IsExist(id);
+        if(!isexist)
             return false;
         employeeRepository.deleteById(id);
         return true;
+    }
+
+    @PatchMapping(path="/{id}")
+    public EmployeeDTO updateByIdPatch(@RequestBody Map<String, Object> updates,@PathVariable Long id){
+
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).orElse(null);
+        if(employeeEntity==null){
+            throw new IllegalArgumentException("Employee not found with id: " + id);
+        }
+
+        updates.forEach((field, value) -> {
+            // Use reflection to find the field in the EmployeeEntity class
+            Field field1 = ReflectionUtils.findField(EmployeeEntity.class, field);
+            if (field1 == null) {
+                return ;
+            }
+            // Make the field accessible for modification
+            field1.setAccessible(true);
+            // Set the new value for the field in the employeeEntity object
+            ReflectionUtils.setField(field1, employeeEntity, value);
+        });
+
+        return modelMapper.map(employeeRepository.save(employeeEntity), EmployeeDTO.class);
+
     }
 }
